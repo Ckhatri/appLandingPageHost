6c464d2a015de90c5b49e56d8fb31ca07ac86918
ckhatri <chirag.k.khatri@gmail.com>
2015-07-20 09:27:21 -0500

    Added a footer, made buttons scrollable

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 $(document).ready(function() {
+	$("#learnbtn").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#welcome").offset().top
+		}, 1000);
+	});
+
+	$("#downloadbtn").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#available").offset().top
+		}, 1000);
+	});
 });
